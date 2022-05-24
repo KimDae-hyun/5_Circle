0a937a3b5747e9539d4936cbcf1efd796c8d9619
KimDae-hyun <dhkim5608@naver.com>
2022-05-24 14:39:38 +0900

    Feat : finish Webserv
    - 웹서브 평가버전
    - 테스트 전 각종 path 변경시켜주기

---
+++ webserv/state_pages/upload.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="ko">
+  <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+      <title>업로드 성공!</title>
+  </head>
+  <body>
+  <?php
+      echo "<p>파일이 업로드 되었습니다 🏖 </p>";
+  ?>
+  </body>
+</html>
